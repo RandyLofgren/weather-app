9448477f65a719942b781a6e3b197b5ae6065557
RandyLofgren <43276499+RandyLofgren@users.noreply.github.com>
2020-11-13 23:20:49 -0500

    got some of the for loop working

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,12 @@
 var cities = [];
 
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+
 function displayCityInfo() {
 
   var city = $(this).attr("data-name");
   var APIKey = "3047b4fdf5e4cef615044702d2f6aa10";
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" + city + "&units=imperial&appid=" + APIKey;
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
 
   // Creates AJAX call for the specific city button being clicked
@@ -17,53 +16,53 @@ function displayCityInfo() {
     method: "GET"
   }).then(function (response) {
 
-    $(".cityapi").text("City: " + response.name);
-    $(".windapi").text("wind: " + response.wind.speed + "mph");
+    var iconGrab = (response.weather[0].icon);
+    var icon = "http://openweathermap.org/img/wn/" + iconGrab + "@2x.png";
+    var myDate = new Date(1605311332 * 1000);
+
+    $("#cloudIcon").attr("src", icon);
+    $(".cityapi").text(response.name + "  ");
+    $(".windapi").text("Wind: " + response.wind.speed + "MPH");
     $(".humidityapi").text("Humidity: " + response.main.humidity + "%");
-    $(".tempapi").text("Temp: " + response.main.temp + " degrees");
-    console.log(response);
-    // $(".lonapi").text("Longitude: " + response.coord.lon)
-    // $(".latapi").text("Latitude: " + response.coord.lat)
+    $(".tempapi").text("Temp: " + response.main.temp + " \u00B0F");
+
+    $("#cityID").append(myDate.toGMTString() + "<br>" + myDate.toLocaleString());
+
     var lon = (response.coord.lon)
     var lat = (response.coord.lat)
 
-     var UVqueryCall =  "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon +"&appid="+ APIKey;
+    var UVqueryCall = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
-     $.ajax({
+    $.ajax({
       url: UVqueryCall,
       method: "GET"
     }).then(function (UVResponse) {
       $(".uv-indexapi").text("UV Index: " + UVResponse.value);
-
-      console.log(UVResponse)
+      if (UVResponse.value < 2.01) {
+        $(".uv-indexapi").addClass(greenBack);
+      } else if (UVResponse.value < 5.01) {
+        $(".uv-indexapi").addClass(yellowBack);
+      } else if (UVResponse.value < 7.01) {
+        $(".uv-indexapi").addClass(orangeBack);
+      } $(".uv-indexapi").addClass(redBack);
     })
 
-    
-
-    // YOUR CODE GOES HERE!!!
 
 
-    // YOUR CODE GOES HERE!!!
 
   });
-  
+
 }
 
-
 function renderButtons() {
-
 
   $("#buttons-view").empty();
   var savedCities = JSON.parse(localStorage.getItem("addedCity"))
   if (savedCities !== null) {
     cities = savedCities
-
   }
 
-
   for (var i = 0; i < cities.length; i++) {
-
-
     var a = $("<button>");
     a.addClass("city");
     a.attr("data-name", cities[i]);
@@ -72,28 +71,74 @@ function renderButtons() {
   }
 }
 
-
 $("#sub-city").on("click", function (event) {
   event.preventDefault();
-
   var cityAdder = $("#add-city").val().trim();
   if (cities.indexOf(cityAdder) === -1) {
     cities.push(cityAdder);
-    localStorage.setItem("addedCity", JSON.stringify(cities))
-    $("#add-city").val("")
+    localStorage.setItem("addedCity", JSON.stringify(cities));
+    $("#add-city").val("");
+
   }
-
-
-
-
-
-
   renderButtons();
-
-
 });
+
+function fiveCityInfo() {
+
+  var city = $(this).attr("data-name");
+  var APIKey = "3047b4fdf5e4cef615044702d2f6aa10";
+  var fiveURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+
+
+  // Creates AJAX call for the specific city button being clicked
+  $.ajax({
+    url: fiveURL,
+    method: "GET"
+  }).then(function (fiveresponse) {
+    console.log(fiveresponse)
+    results = fiveresponse.list
+
+    for (var i = 0; i < 39; i = i + 8) {
+      fiveDayDiv = $("<div>");
+      p = $("<p>");
+      p.text(results[i].dt);
+      fiveDayDiv.addClass("col");
+      fiveDayDiv.addClass("five-day")
+      fiveDayDiv.addClass("rounded")
+      fiveDayDiv.append(p);
+      $("#fiveDayCast").append(fiveDayDiv);
+      cloudImage = $("<img>")
+      cloudImage.attr("src", results[i].weather.icon)
+      fiveDayDiv.append(cloudImage)
+      // ptemp= $("<p>")
+      // ptemp.text("Temp: " + results[i].main.temp + "\u00B0F")
+      // $(".five-day").append(ptemp)
+
+
+
+    }
+
+
+    // // Make an image tag with jQuery and store it in a variable named animalImage.
+    // animalImage = $("<img>")
+    // // Set the image's src to results[i]'s fixed_height.url.
+    // animalImage.attr("src", results[i].images.fixed_height.url);
+    // animalDiv.append(animalImage)
+
+
+
+  });
+
+}
+
+
+
 
 
 $(document).on("click", ".city", displayCityInfo);
+$(document).on("click", ".city", fiveCityInfo);
+
+
 
 renderButtons();
+
